597a7b2d60d08540add0755568a42afde1e30f78
Santosh Shinde <santoshshinde2012@gmail.com>
2024-05-25 15:55:11 +0530

    Added unit test cases for app and routes, fixed prettier issues

--- tests/unit-tests/components/enquiry/EnquiryController.spec.ts
+++ tests/unit-tests/components/enquiry/EnquiryController.spec.ts
@@ -8,185 +8,223 @@ import ApiError from '../../../../src/abstractions/ApiError';
 import { RouteDefinition } from '../../../../src/types/RouteDefinition';
 
 describe('Enquiry Controller', () => {
-    let request: Partial<Request>;
-    let response: Partial<Response>;
-    let next: NextFunction = jest.fn();
-    let controller: EnquiryController;
+	let request: Partial<Request>;
+	let response: Partial<Response>;
+	let next: NextFunction = jest.fn();
+	let controller: EnquiryController;
 
-    beforeAll(() => {
-        controller = new EnquiryController();
-    });
+	beforeAll(() => {
+		controller = new EnquiryController();
+	});
 
-    beforeEach(() => {
-        request = {} as Partial<Request>;
-        response = {
-            locals: {},
-            status: jest.fn(),
-            send: jest.fn()
-        } as Partial<Response>;
-    });
+	beforeEach(() => {
+		request = {} as Partial<Request>;
+		response = {
+			locals: {},
+			status: jest.fn(),
+			send: jest.fn(),
+		} as Partial<Response>;
+	});
 
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
 
-    it('should defined routes and return array of route definition', () => {
-        const routes: RouteDefinition[] = controller.routes();
-        expect(routes).toBeDefined();
-        expect(routes.length).toBeGreaterThan(0);
-    });
+	it('should defined routes and return array of route definition', () => {
+		const routes: RouteDefinition[] = controller.routes();
+		expect(routes).toBeDefined();
+		expect(routes.length).toBeGreaterThan(0);
+	});
 
-    it('should get all enquiries', async () => {
-        const getAll = jest.spyOn(EnquiryService.prototype, 'getAll');
-        getAll.mockResolvedValueOnce(enquiries);
+	it('should get all enquiries', async () => {
+		const getAll = jest.spyOn(EnquiryService.prototype, 'getAll');
+		getAll.mockResolvedValueOnce(enquiries);
 
-        await controller.getEnquiries(request as Request, response as Response, next);
+		await controller.getEnquiries(
+			request as Request,
+			response as Response,
+			next,
+		);
 
-        const locals = response.locals;
-        expect(locals?.data).toBeDefined();
-    });
+		const locals = response.locals;
+		expect(locals?.data).toBeDefined();
+	});
 
-    it('should handle errors to get all enquiries', async () => {
-        const error = new Error('Internal Server Error');
-        const getAll = jest.spyOn(EnquiryService.prototype, 'getAll');
-        getAll.mockRejectedValueOnce(error);
+	it('should handle errors to get all enquiries', async () => {
+		const error = new Error('Internal Server Error');
+		const getAll = jest.spyOn(EnquiryService.prototype, 'getAll');
+		getAll.mockRejectedValueOnce(error);
 
-        await controller.getEnquiries(request as Request, response as Response, next);
+		await controller.getEnquiries(
+			request as Request,
+			response as Response,
+			next,
+		);
 
-        expect(next).toHaveBeenCalled();
-    });
+		expect(next).toHaveBeenCalled();
+	});
 
-    it('should get details of a specific enquiry', async () => {
-        const [enquiry] = enquiries as EnquiryAttributes[];
-        const getById = jest.spyOn(EnquiryService.prototype, 'getById');
-        getById.mockResolvedValueOnce(enquiry);
+	it('should get details of a specific enquiry', async () => {
+		const [enquiry] = enquiries as EnquiryAttributes[];
+		const getById = jest.spyOn(EnquiryService.prototype, 'getById');
+		getById.mockResolvedValueOnce(enquiry);
 
-        request.params = { id: '1' };
+		request.params = { id: '1' };
 
-        await controller.getEnquiry(request as Request, response as Response, next);
+		await controller.getEnquiry(
+			request as Request,
+			response as Response,
+			next,
+		);
 
-        const locals = response.locals;
+		const locals = response.locals;
 
-        expect(getById).toHaveBeenCalledWith('1');;
-        expect(locals?.data).toBeDefined();
-    });
+		expect(getById).toHaveBeenCalledWith('1');
+		expect(locals?.data).toBeDefined();
+	});
 
-    it('should handle errors to get all enquiries', async () => {
-        const error = new Error('Internal Server Error');
-        const getById = jest.spyOn(EnquiryService.prototype, 'getById');
-        getById.mockRejectedValueOnce(error);
+	it('should handle errors to get all enquiries', async () => {
+		const error = new Error('Internal Server Error');
+		const getById = jest.spyOn(EnquiryService.prototype, 'getById');
+		getById.mockRejectedValueOnce(error);
 
-        await controller.getEnquiry(request as Request, response as Response, next)
+		await controller.getEnquiry(
+			request as Request,
+			response as Response,
+			next,
+		);
 
-        expect(next).toHaveBeenCalled();
-    });
+		expect(next).toHaveBeenCalled();
+	});
 
-    it('should create enquiry', async () => {
-        const [enquiry] = enquiries as EnquiryAttributes[];
-        const body = { name: 'New Org' };
-        enquiry.name = 'New Org';
-        const create = jest.spyOn(EnquiryService.prototype, 'create');
-        create.mockResolvedValueOnce(enquiry);
+	it('should create enquiry', async () => {
+		const [enquiry] = enquiries as EnquiryAttributes[];
+		const body = { name: 'New Org' };
+		enquiry.name = 'New Org';
+		const create = jest.spyOn(EnquiryService.prototype, 'create');
+		create.mockResolvedValueOnce(enquiry);
 
-        request.body = body;
+		request.body = body;
 
-        await controller.createEnquiry(request as Request, response as Response, next);
+		await controller.createEnquiry(
+			request as Request,
+			response as Response,
+			next,
+		);
 
-        expect(create).toHaveBeenCalledWith(body);
-        const locals = response.locals;
-        expect(locals?.data).toEqual({ enquiry });
-        expect(response.status).toHaveBeenCalledWith(StatusCodes.CREATED);
-    });
+		expect(create).toHaveBeenCalledWith(body);
+		const locals = response.locals;
+		expect(locals?.data).toEqual({ enquiry });
+		expect(response.status).toHaveBeenCalledWith(StatusCodes.CREATED);
+	});
 
-    it('should throw BAD_REQUEST error if name and country are not provided', async () => {
-        request.body = {};
+	it('should throw BAD_REQUEST error if name and country are not provided', async () => {
+		request.body = {};
 
-        await controller.createEnquiry(request as Request, response as Response, next);
+		await controller.createEnquiry(
+			request as Request,
+			response as Response,
+			next,
+		);
 
-        expect(next).toHaveBeenCalledWith(new ApiError(ReasonPhrases.BAD_REQUEST, StatusCodes.BAD_REQUEST));
-    });
+		expect(next).toHaveBeenCalledWith(
+			new ApiError(ReasonPhrases.BAD_REQUEST, StatusCodes.BAD_REQUEST),
+		);
+	});
 
-    it('should handle errors in create enquiry', async () => {
-        const body = { name: 'New Org' };
-        const error = new Error('Internal Server Error');
-        const create = jest.spyOn(EnquiryService.prototype, 'create');
-        create.mockRejectedValueOnce(error);
+	it('should handle errors in create enquiry', async () => {
+		const body = { name: 'New Org' };
+		const error = new Error('Internal Server Error');
+		const create = jest.spyOn(EnquiryService.prototype, 'create');
+		create.mockRejectedValueOnce(error);
 
-        request.body = body;
+		request.body = body;
 
-        await controller.createEnquiry(request as Request, response as Response, next);
+		await controller.createEnquiry(
+			request as Request,
+			response as Response,
+			next,
+		);
 
-        expect(create).toHaveBeenCalledWith(body);
-        expect(next).toHaveBeenCalledWith(error);
-    });
+		expect(create).toHaveBeenCalledWith(body);
+		expect(next).toHaveBeenCalledWith(error);
+	});
 
-    it('should update enquiry', async () => {
-        const [enquiry] = enquiries as EnquiryAttributes[];
-        const { id } = enquiry;
-        const body = { name: 'Updated Org' };
-        enquiry.name = 'Updated Org';
+	it('should update enquiry', async () => {
+		const [enquiry] = enquiries as EnquiryAttributes[];
+		const { id } = enquiry;
+		const body = { name: 'Updated Org' };
+		enquiry.name = 'Updated Org';
 
-        const update = jest.spyOn(EnquiryService.prototype, 'update');
-        update.mockResolvedValueOnce(enquiry);
+		const update = jest.spyOn(EnquiryService.prototype, 'update');
+		update.mockResolvedValueOnce(enquiry);
 
-        request.params = { id };
-        request.body = body;
+		request.params = { id };
+		request.body = body;
 
-        await controller.updateEnquiry(request as Request, response as Response, next);
+		await controller.updateEnquiry(
+			request as Request,
+			response as Response,
+			next,
+		);
 
-        expect(update).toHaveBeenCalledWith(id, body);
-        const locals = response.locals;
-        expect(locals?.data).toEqual({ enquiry });
-    });
+		expect(update).toHaveBeenCalledWith(id, body);
+		const locals = response.locals;
+		expect(locals?.data).toEqual({ enquiry });
+	});
 
-    it('should handle errors in update enquiry', async () => {
-        const [enquiry] = enquiries as EnquiryAttributes[];
-        const { id } = enquiry;
-        const body = { name: 'Updated Org' };
-        const error = new Error('Internal Server Error');
+	it('should handle errors in update enquiry', async () => {
+		const [enquiry] = enquiries as EnquiryAttributes[];
+		const { id } = enquiry;
+		const body = { name: 'Updated Org' };
+		const error = new Error('Internal Server Error');
 
-        const update = jest.spyOn(EnquiryService.prototype, 'update');
-        update.mockRejectedValueOnce(error);
+		const update = jest.spyOn(EnquiryService.prototype, 'update');
+		update.mockRejectedValueOnce(error);
 
-        request.params = { id };
-        request.body = body;
+		request.params = { id };
+		request.body = body;
 
-        await controller.updateEnquiry(request as Request, response as Response, next);
+		await controller.updateEnquiry(
+			request as Request,
+			response as Response,
+			next,
+		);
 
-        expect(update).toHaveBeenCalledWith(id, body);
-        expect(next).toHaveBeenCalledWith(error);
-    });
+		expect(update).toHaveBeenCalledWith(id, body);
+		expect(next).toHaveBeenCalledWith(error);
+	});
 
-    it('should delete enquiry', async () => {
-        const [enquiry] = enquiries as EnquiryAttributes[];
-        const { id } = enquiry;
-        const status = true;
+	it('should delete enquiry', async () => {
+		const [enquiry] = enquiries as EnquiryAttributes[];
+		const { id } = enquiry;
+		const status = true;
 
-        const remove = jest.spyOn(EnquiryService.prototype, 'delete');
-        remove.mockResolvedValueOnce(status);
+		const remove = jest.spyOn(EnquiryService.prototype, 'delete');
+		remove.mockResolvedValueOnce(status);
 
-        request.params = { id };
+		request.params = { id };
 
-        await controller.delete(request as Request, response as Response, next);
+		await controller.delete(request as Request, response as Response, next);
 
-        expect(remove).toHaveBeenCalledWith(id);
-        const locals = response.locals;
-        expect(locals?.data).toEqual({ status });
-    });
+		expect(remove).toHaveBeenCalledWith(id);
+		const locals = response.locals;
+		expect(locals?.data).toEqual({ status });
+	});
 
-    it('should handle errors in delete enquiry', async () => {
-        const [enquiry] = enquiries as EnquiryAttributes[];
-        const { id } = enquiry;
-        const error = new Error('Internal Server Error');
+	it('should handle errors in delete enquiry', async () => {
+		const [enquiry] = enquiries as EnquiryAttributes[];
+		const { id } = enquiry;
+		const error = new Error('Internal Server Error');
 
-        const remove = jest.spyOn(EnquiryService.prototype, 'delete');
-        remove.mockRejectedValueOnce(error);
+		const remove = jest.spyOn(EnquiryService.prototype, 'delete');
+		remove.mockRejectedValueOnce(error);
 
-        request.params = { id };
+		request.params = { id };
 
-        await controller.delete(request as Request, response as Response, next);
+		await controller.delete(request as Request, response as Response, next);
 
-        expect(remove).toHaveBeenCalledWith(id);
-        expect(next).toHaveBeenCalledWith(error);
-    });
+		expect(remove).toHaveBeenCalledWith(id);
+		expect(next).toHaveBeenCalledWith(error);
+	});
 });
